4c9a8682a20aa97fc0ddcd8be37766cba2b09cff
CXY573 <1391642270@qq.com>
2018-03-15 09:13:24 +0800

    Update Easy part.cpp

--- Easy part.cpp
+++ Easy part.cpp
@@ -113,3 +113,30 @@ public:
             return len;
     }
 };
+
+7. Count and Say
+class Solution 
+{
+public:
+    string countAndSay(int n)
+    {
+        if(n==0) return "";
+        string res="1"; 
+        while(--n)
+        {
+            string cur="";
+            for(int i=0;i<res.size();i++)
+            {
+                int count=1;
+                while(res[i]==res[i+1])
+                {
+                    count++;
+                    i++;
+                }
+                cur=cur+to_string(count)+res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
