27a7ea069c746791c9c2d692c68e437373ecb3dc
CXY573 <1391642270@qq.com>
2018-03-16 10:55:53 +0800

    Update Easy part.cpp

--- Easy part.cpp
+++ Easy part.cpp
@@ -140,3 +140,25 @@ public:
         return res;
     }
 };
+
+8.Valid Parentheses
+#include <stack>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char& c : s) {
+            switch (c) {
+                case '(': 
+                case '{': 
+                case '[': paren.push(c); break;
+                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                default: ; // pass
+            }
+        }
+        return paren.empty() ;
+    }
+};
