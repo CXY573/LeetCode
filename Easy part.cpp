9c6a95ee16da943af49e2b648e27bcb8f774db8a
CXY573 <1391642270@qq.com>
2018-03-19 09:21:04 +0800

    Update Easy part.cpp

--- Easy part.cpp
+++ Easy part.cpp
@@ -162,3 +162,22 @@ public:
         return paren.empty() ;
     }
 };
+
+
+9. Remove Duplicates from Sorted Array
+class Solution 
+{
+public:
+   int removeDuplicates(vector<int>& nums) 
+   {
+    int pos = 0;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (i == 0 || nums[i] != nums[pos - 1])
+            nums[pos++] = nums[i];
+    }
+
+    return pos;
+    }
+};
