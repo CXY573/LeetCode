c87ad1cc5e6efb7a333b4a3d57f1d6d85319cb84
CXY573 <1391642270@qq.com>
2018-03-20 09:16:27 +0800

    Update Easy part.cpp

--- Easy part.cpp
+++ Easy part.cpp
@@ -187,3 +187,21 @@ public:
        return nums.size();
 }
 };
+
+10. Plus One
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        bool carry = true;
+        
+        for(int i=digits.size()-1; i >= 0 && carry; i--) {
+            carry = (++digits[i]%=10) == 0;
+        }
+
+        if(carry) {
+            digits.insert(digits.begin(), 1);
+        }
+    
+        return digits;
+    }
+};
