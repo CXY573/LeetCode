1577d276ebeff5705c79ee2c3271724fd9c5b8d9
CXY573 <1391642270@qq.com>
2018-03-22 09:00:40 +0800

    Update Easy part.cpp

--- Easy part.cpp
+++ Easy part.cpp
@@ -233,3 +233,27 @@ public:
     }
         
 };
+
+12. Sqrt(x)
+class Solution 
+{
+public:
+    int mySqrt(int x) 
+   {
+ if (x == 0)
+        return 0;
+    int left = 1, right = 2147483648;
+    while (true) {
+        int mid = left + (right - left)/2;
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+    }
+};
+
+
